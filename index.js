e336ef4c8fa9b16c31588aef551bd9849418c1bb
Louis Pazos <louispazos@gmail.com>
2017-11-23 07:33:28 +0000

    twitter and facebook button

--- index.js
+++ index.js
@@ -31,9 +31,13 @@ document.getElementById("btnquotes").onclick = function() {
         // place.innerHTML = musk.inspiration[1].quote;
         place.innerHTML = musk.inspiration[Math.floor(Math.random() * 4)].quote;
 
-}
+};
 
-
-// function random() {
-//     document.getElementById("quotes").innerHTML = musk.inspiration
-// }
+ (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
