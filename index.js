cb5359b7d578cc42f17e17014e0a703933062bc3
Louis Pazos <louispazos@gmail.com>
2017-12-05 23:43:47 +0000

    turn on facebook

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ document.getElementById("btnquotes").onclick = function() {
 
 
 
-//  (function(d, s, id) {
-//                 var js, fjs = d.getElementsByTagName(s)[0];
-//                 if (d.getElementById(id)) return;
-//                 js = d.createElement(s);
-//                 js.id = id;
-//                 js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
-//                 fjs.parentNode.insertBefore(js, fjs);
-//             }(document, 'script', 'facebook-jssdk'));
+ (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
             
          
 document.getElementById("tweet").onclick = function(){
